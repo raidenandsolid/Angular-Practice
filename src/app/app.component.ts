8d9f2adcc22eb1a1b3a5c7b7b5e3ee97e29a46ef
Daniel <raidenandsold@gmail.com>
2022-08-27 10:23:59 -0500

    First commit for angular practice

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// git 
+@Component({
+  selector: 'am-root',
+  templateUrl: './complete.component.html',
+  styleUrls: ['./complete.component.css']
+})
+export class AppComponent {
+  title = 'complete-new';
+}
