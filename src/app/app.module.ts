cbe66052dbdff51fb2146d4d6d6f03a82daaaa7f
Vernon Williams <voxenn@gmail.com>
2022-09-02 12:54:18 -0500

    Revert "Add routing (#4)"
    This reverts commit c8b65c809af447cc6a5a25efbc0e79d207e3c92e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { appRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { FormComponent } from '../form/form.component';
 import { FooterComponent } from '../footer/footer.component';
-import { RegisterComponent } from './register';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    FooterComponent,
-    RegisterComponent
+    FooterComponent
   ],
   imports: [
-    BrowserModule,
-    appRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
