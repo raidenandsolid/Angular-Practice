a767cd56e95c2e0177815d902b14cdf7eb81ce0c
Vernon Williams <voxenn@gmail.com>
2022-09-09 14:53:37 -0500

    Active development (#9)
    * Change submit button name
    
    * Add routing (#4)
    
    * Revert "Add routing (#4)" (#5)
    
    This reverts commit c8b65c809af447cc6a5a25efbc0e79d207e3c92e.
    
    * Add Login Page component (#6)
    
    * Separate footer into its own component.
    
    * Add Login Page component
    
    * Add changes
    
    Co-authored-by: Daniel <raidenandsold@gmail.com>
    
    * Add routing (#7)
    
    * Login page (#8)
    
    * Separate footer into its own component.
    
    * Add Login Page component
    
    * Add changes
    
    * Implement routing between Home, Login, and Register pages
    
    * Update app.component.html
    
    Deleted commented code.
    
    Co-authored-by: Daniel <raidenandsold@gmail.com>
    
    Co-authored-by: Daniel Vandagriff <daniel_vandagriff@yahoo.com>
    Co-authored-by: Daniel <raidenandsold@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { routingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { FormComponent } from '../form/form.component';
 import { FooterComponent } from '../footer/footer.component';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
