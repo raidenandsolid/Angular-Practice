cb0f6799720c7066a265cc9211594cad77b1e5be
Daniel <raidenandsold@gmail.com>
2022-09-09 14:41:49 -0500

    Implement routing between Home, Login, and Register pages

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 
